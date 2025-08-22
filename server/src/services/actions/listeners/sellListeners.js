2fb7d762796f3a290452889900a83fd26cc1c277
Víctor <Victor@MacBook-Air-de-Victor.local>
2025-08-22 22:58:00 +0200

    Add the two 3_3weth strategies

--- server/src/services/actions/listeners/sellListeners.js
+++ server/src/services/actions/listeners/sellListeners.js
@@ -2,6 +2,8 @@ import { eventEmitter } from "../../../utils/eventEmitter.js";
 import sellAfternoons2_2 from "../strategies/whales/highFrequency/equals/afternoons2_2/sellAfternoons2_2.js";
 import sell2_2wEth from "../strategies/whales/wEth/equals/weth2_2/sell2_2wEth.js";
 import sell2_2weth130 from "../strategies/whales/wEth/equals/weth2_2_130/sell2_2wEth130.js";
+import sell3_3wEth from "../strategies/whales/wEth/equals/weth3_3/sell3_3wEth.js";
+import sell3_3weth130 from "../strategies/whales/wEth/equals/weth3_3_130/sell3_3wEth130.js";
 import isOperationInTable from "../utils/isOperationInTable.js";
 
 
@@ -27,9 +29,18 @@ const sellListeners = async () => {
             console.log('LUZ VERDE PARA VENDER 2_2weth!!! a: ', sellPrice)
             await sell2_2wEth();
         }
+
+        const isOperation3_3Weth = await isOperationInTable("3_3weth");
+
+        if (isOperation3_3Weth === 'BUY') {
+            console.log('LUZ VERDE PARA VENDER 3_3weth!!! a: ', sellPrice)
+            await sell3_3wEth();
+        }
     })
 
-        eventEmitter.on('highTradeSignalWeth130', async (sellPrice) => {
+    
+
+    eventEmitter.on('highTradeSignalWeth130', async (sellPrice) => {
         console.log(`Se emitió un highTradeSignalWeth130 con sellPrice: ${sellPrice}`);
 
         const isOperation2_2Weth = await isOperationInTable("2_2weth130");
@@ -37,6 +48,13 @@ const sellListeners = async () => {
         if (isOperation2_2Weth === 'BUY') {
             console.log('LUZ VERDE PARA VENDER 2_2weth130!!! a: ', sellPrice);
            await sell2_2weth130();
+        }
+
+        const isOperation3_3Weth = await isOperationInTable("3_3weth130");
+
+        if (isOperation3_3Weth === 'BUY') {
+            console.log('LUZ VERDE PARA VENDER 3_3weth130!!! a: ', sellPrice);
+           await sell3_3weth130();
         }
     })
 
